b1c1011a390c119c534de5933e8dfa7e150a8c83
arthurlongue <arthurlongue@hotmail.com>
2026-04-11 19:00:52 -0300

    feat(frontend): implement root layout, global header, and 404 page

--- frontend/src/components/layout/auth-layout.tsx
+++ frontend/src/components/layout/auth-layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 
 export function AuthLayout() {
 	return (
-		<div className="flex min-h-screen flex-col justify-center bg-muted/40">
+		<div className="flex flex-1 flex-col justify-center bg-muted/40">
 			<Outlet />
 		</div>
 	)
